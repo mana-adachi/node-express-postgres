778339f771a4dcd62a27d60b42c0ff0f0acb8e51
mana-adachi <166688438+mana-adachi@users.noreply.github.com>
2024-06-20 07:14:32 +0000

    第１３章の変更

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,37 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const pg = require('pg');
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+let todos = [];
+
+
+const connection = new pg.Pool({
+  host: 'localhost',
+  user: 'root',
+  password: 'postgres',
+  database: 'todo_app',
+  port: 5432,
+  });
+
+
+  console.log(results.rows)
+  {
+  res.render('index', {
+    title: 'ToDo App',
+    todos: results.rows;
+  });
+};
+
+router.post('/', function (req, res, next) {
+  connection.connect((err) => {
+    if (err) {
+      console.log('error connecting: ' + err.stack);
+      return
+    }
+    console.log('success');
+  });
+  const todo = req.body.add;
+  res.redirect('/');
 });
 
 module.exports = router;
